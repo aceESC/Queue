0666a67adc9213ccf6f2e5a6d09a16a52408acb6
ace <ace@aces-mac-mini.electronicscience.com>
2019-06-11 11:30:31 +0800

    test

---
+++ Test Suites/Test.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6b1e03d9-d328-40ca-ac49-1c15fbb76ec0</testSuiteGuid>
+   <testCaseLink>
+      <guid>d693dcea-7cd1-4de8-9c81-762ea42e461f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/FCFS/QMF-003</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
